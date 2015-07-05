a431067442e25bfa2e6bdeefdd32023518d189ba
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2015-07-05 15:25:30 +1000

    Refactored and tested starter code

--- flow/starter.rb
+++ flow/starter.rb
@@ -4,13 +4,48 @@ require_relative 'activities'
 require_relative 'workflows'
 
 class Starter
-  attr_reader :runner_file
+  RETENTION_PERIOD = 1
+
+  attr_reader :runner_file, :swf, :domain, :client
   def initialize(runner_file = 'worker.json')
     @runner_file = runner_file
+    @swf         = AWS::SimpleWorkflow.new
+    @domain      = setup_domain
+    @client      = setup_client
+  end
+
+  def setup_domain
+    domain = swf.domains[domain_name]
+    swf.domains.create(domain_name, RETENTION_PERIOD) unless domain.exists?
+    domain
   end
 
   def domain_name
     @domain_name ||= options.fetch('domain').fetch('name')
+  end
+
+  def workflow_workers(worker = 0)
+    options.fetch('workflow_workers')[worker]
+  end
+
+  def task_list_name(worker = 0)
+    workflow_workers(worker).fetch('task_list')
+  end
+
+  def setup_client
+    @client ||= AWS::Flow::workflow_client(domain.client, domain) do
+      { from_class: 'ChannelrWorkflow',
+        task_list: task_list_name }
+    end
+  end
+
+  def start
+    puts "Starting a workflow execution."
+    puts "  domain:     #{domain_name}"
+    puts "  task list:  #{task_list_name}"
+
+    # start the workflow, passing it a name to use.
+    client.start_execution("AWS OpsWorks")
   end
 
   private
@@ -20,20 +55,22 @@ class Starter
   end
 end
 
-def setup_domain(domain_name, retention_period)
-  swf = AWS::SimpleWorkflow.new
-  domain = swf.domains[domain_name]
-  unless domain.exists?
-    swf.domains.create(domain_name, retention_period)
-  end
-  domain
-end
+Starter.new.start
+
+# def setup_domain(domain_name, retention_period)
+#   swf = AWS::SimpleWorkflow.new
+#   domain = swf.domains[domain_name]
+#   unless domain.exists?
+#     swf.domains.create(domain_name, retention_period)
+#   end
+#   domain
+# end
 
 # get the path to the runner configuration file.
-if ARGV.length < 1
+# if ARGV.length < 1
   # puts "Please provide the path to the runner configuration file!"
   # exit
-end
+# end
 # runner_spec = ARGV[0]
 #
 # # Read the domain info from the same JSON file that the runner will be using.
--- spec/starter_spec.rb
+++ spec/starter_spec.rb
@@ -1,18 +1,70 @@
 require_relative 'spec_helper'
 # require_relative '../flow/starter.rb'
 
+# class Starter::SimpleWorkflowStub
+#   def domains
+#     self
+#   end
+#
+#   def [](domain)
+#     ['EXSISTING_DOMAIN'].include?(domain) ? Starter::Domain.new : Starter::NullDomain.new
+#   end
+#
+#   def create
+#
+#   end
+# end
+#
+# class Starter::NullDomain
+#   def exists?
+#     true
+#   end
+# end
+#
+# class Starter::NullDomain
+#   def exists?
+#     false
+#   end
+# end
+
+class Starter::Domain
+end
+
+class Starter::Client
+end
+
 RSpec.describe 'Starter' do
-  let(:subject) { Starter.new }
+  let(:test_runner) {'spec/fixtures/test.json'}
+  let(:subject)     {Starter.new(test_runner)}
+
+  before do
+    allow(subject).to receive(:setup_domain) { Starter::Domain.new }
+    allow(subject).to receive(:setup_client) { Starter::Client.new }
+  end
+
+  # before { allow(AWS::SimpleWorkflow).to receive(:new) { Starter::SimpleWorkflowStub.new } }
 
   describe '#domain_name' do
     it 'parses the domain name' do
-      expect(subject.domain_name).to eq('Channelr')
-    end
-
-    it 'uses a runner file to parse the domain' do
-      test_runner = 'spec/fixtures/test.json'
-      subject     = Starter.new(test_runner)
       expect(subject.domain_name).to eq('FIXTURE_DOMAIN')
     end
   end
+
+  describe '#workflow_workers' do
+    it 'parses the workflow workers' do
+      first_task_list = {"task_list" => 'FIRST_TASK_LIST'}
+      expect(subject.workflow_workers).to eq(first_task_list)
+    end
+  end
+
+  describe '#task_list_name' do
+    it 'parses the task list name' do
+      expect(subject.task_list_name).to eq('FIRST_TASK_LIST')
+    end
+  end
+
+  # client = AWS::Flow::workflow_client(domain.client, domain) {
+  #   { from_class: "ChannelrWorkflow",
+  #     task_list: task_list_name }
+  # }
 end
